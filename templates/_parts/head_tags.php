a6a5b2930fc7d14438c846fcc48343fc19ee8c36
Hiroyuki Suzuki <github@mofg.net>
2018-01-27 22:57:11 +0900

    分析タグの配置場所を変更

--- templates/_parts/head_tags.php
+++ templates/_parts/head_tags.php
@@ -1,3 +1,3 @@
 <?php
-$filePath = DOC_ROOT."/include/google_analytics.txt";
+$filePath = DOC_ROOT."/../google_analytics.txt";
 if( file_exists($filePath) && is_readable($filePath) ) echo file_get_contents($filePath);
