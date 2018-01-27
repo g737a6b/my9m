1b2d23aa7c708f69d9515bdd382ed394e57cefac
Hiroyuki Suzuki <github@mofg.net>
2018-01-27 17:31:18 +0900

    フッターリンクの追加

--- templates/_parts/footer.php
+++ templates/_parts/footer.php
@@ -9,6 +9,9 @@
 			<li class="footer-nav__item">
 				<a href="https://mofg.net" class="footer-nav-link">運営者</a>
 			</li>
+			<li class="footer-nav__item">
+				<a href="https://github.com/g737a6b/my9m" class="footer-nav-link">貢献</a>
+			</li>
 		</ul>
 		<div class="footer-copyright">(c) <?php echo date("Y"); ?> My9</div>
 	</footer><!-- .footer -->
