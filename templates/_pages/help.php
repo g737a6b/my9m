26c674de9085c4dc876127aec5ff2de48a3aa1f8
Hiroyuki Suzuki <github@mofg.net>
2018-02-03 13:45:08 +0900

    ヘルプページの更新

--- templates/_pages/help.php
+++ templates/_pages/help.php
@@ -7,9 +7,14 @@
 		<h2 class="heading-01 mt30">よくある質問</h2>
 		<ul class="list-01 mt20">
 			<li class="list-01__item"><a href="#q1000" class="color-link-blue effect-tdu-on-hover">登録したデータはどこに保存されますか?</a></li>
+			<li class="list-01__item"><a href="#q2000" class="color-link-blue effect-tdu-on-hover">異なる端末でも同じタスクリストを使用できますか?</a></li>
 		</ul>
 		<h3 id="q1000" class="heading-02 mt40">登録したデータはどこに保存されますか?</h3>
 		<div class="mt20">
 			<p>ご利用の端末に保存されます(LocalStorage)。ブラウザの設定から履歴削除などを行うと一緒に削除される場合がありますのでご注意ください。</p>
+		</div>
+		<h3 id="q2000" class="heading-02 mt40">異なる端末でも同じタスクリストを使用できますか?</h3>
+		<div class="mt20">
+			<p>できません。短い時間で全てを完了できる量を登録することで、最も大きな効果が得られます。</p>
 		</div>
 	</main><!-- .main -->
