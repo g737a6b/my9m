648e6c1cb31eb48dd2f8a80b7012006d0d4a1fbe
Hiroyuki Suzuki <github@mofg.net>
2018-02-03 12:34:26 +0900

    タスクのタイトルを変更できるようにする

--- assets/js/vue_components/app_list.vue
+++ assets/js/vue_components/app_list.vue
@@ -3,7 +3,11 @@
 	<div class="logo font-01"><router-link to="./"><i class="fa fa-angle-left color-lighter-gray fz30"></i> My9</router-link></div>
 	<div v-if="items.length" class="task-list">
 		<draggable v-model="items" @end="updateOrder">
-			<div v-for="(item, index) in items" :key="item.created" class="task-list__item task-list-item">{{item.text}} <a href="#" @click.prevent="removeTask(index)" class="task-list-item__close"><i class="fa fa-close"></i></a></div>
+			<div v-for="(item, index) in items" :key="item.created" class="task-list__item task-list-item">
+				<span>{{item.text}}</span>
+				<a href="#" @click.prevent="renameTask(index)" class="task-list-item__rename"><i class="fa fa-edit"></i></a>
+				<a href="#" @click.prevent="removeTask(index)" class="task-list-item__remove"><i class="fa fa-close"></i></a>
+			</div>
 		</draggable>
 	</div>
 	<p v-else class="empty-tasks">登録されているタスクはありません</p>
@@ -28,6 +32,13 @@ export default {
 		draggable
 	},
 	methods: {
+		renameTask: function(index){
+			let name = window.prompt("タスクのタイトルを変更", this.items[index].text);
+			if( typeof name !== "string" ) return;
+			if( name.trim() === "" ) return this.removeTask(index);
+			this.items[index].text = name.trim();
+			this.$emit("update-storage", this.items);
+		},
 		removeTask: function(index){
 			this.items.splice(index, 1);
 			this.$emit("update-storage", this.items);
