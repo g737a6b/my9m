b566ae992ea70306f0436195a6d7da36147a8111
Hiroyuki Suzuki <github@mofg.net>
2018-02-03 12:18:15 +0900

    Addページでテキストフィールドへフォーカス

--- assets/js/vue_components/app_add.vue
+++ assets/js/vue_components/app_add.vue
@@ -3,7 +3,7 @@
 	<div class="logo font-01"><router-link to="./"><i class="fa fa-angle-left color-lighter-gray fz30"></i> My9</router-link></div>
 	<p v-if="statusSuccess" class="status-label status-label--success mb10"><i class="fa fa-check"></i> {{statusSuccess}} <a href="#" @click.prevent="resetStatus" class="status-label__close"><i class="fa fa-close"></i></a></p>
 	<form action="#" @submit.prevent="addNewTask">
-		<input v-model="newTask" @keyup="resetStatusOnKeyUp" placeholder="タスクを入力してください" class="input-01" />
+		<input v-model="newTask" @keyup="resetStatusOnKeyUp" placeholder="タスクを入力してください" class="input-01" ref="input" />
 		<button type="button" @click="addNewTask" class="button-01 mt10" :class="{'is-disabled': !newTask.length}"><i class="fa fa-plus"></i> 追加する</button>
 	</form>
 </div>
@@ -21,6 +21,7 @@ export default {
 	},
 	mounted: function(){
 		this.statusSuccess = "";
+		this.$refs.input.focus();
 	},
 	methods: {
 		addNewTask: function(){
