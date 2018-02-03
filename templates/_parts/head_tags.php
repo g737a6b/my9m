b85ce4306b74d82c0f73222fe16fd817ce20dbcc
Hiroyuki Suzuki <github@mofg.net>
2018-02-03 13:47:53 +0900

    外部タグ埋め込みファイル名の変更

--- templates/_parts/head_tags.php
+++ templates/_parts/head_tags.php
@@ -1,3 +1,3 @@
 <?php
-$filePath = DOC_ROOT."/../google_analytics.txt";
+$filePath = DOC_ROOT."/../my9m-tags.txt";
 if( file_exists($filePath) && is_readable($filePath) ) echo file_get_contents($filePath);
