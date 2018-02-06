fb1d0c4fd3815fc80500fb4f79cb1321d03a2f16
Hiroyuki Suzuki <github@mofg.net>
2018-02-06 21:50:44 +0900

    ヘルプページの更新

--- templates/_pages/help.php
+++ templates/_pages/help.php
@@ -8,6 +8,7 @@
 		<ul class="list-01 mt20">
 			<li class="list-01__item"><a href="#q1000" class="color-link-blue effect-tdu-on-hover">登録したデータはどこに保存されますか?</a></li>
 			<li class="list-01__item"><a href="#q2000" class="color-link-blue effect-tdu-on-hover">異なる端末でも同じタスクリストを使用できますか?</a></li>
+			<li class="list-01__item"><a href="#q3000" class="color-link-blue effect-tdu-on-hover">利用できるキーボードショートカットを教えてください</a></li>
 		</ul>
 		<h3 id="q1000" class="heading-02 mt40">登録したデータはどこに保存されますか?</h3>
 		<div class="mt20">
@@ -16,5 +17,25 @@
 		<h3 id="q2000" class="heading-02 mt40">異なる端末でも同じタスクリストを使用できますか?</h3>
 		<div class="mt20">
 			<p>できません。短い時間で全てを完了できる量を登録することで、最も大きな効果が得られます。</p>
+		</div>
+		<h3 id="q3000" class="heading-02 mt40">利用できるキーボードショートカットを教えてください</h3>
+		<div class="mt20">
+			<p>PCからご利用の場合は、以下のキーボードショートカットを使用できます。</p>
+			<table class="keyboard-shortcuts-table mt20">
+				<tbody>
+					<tr>
+						<th class="keyboard-shortcuts-table__keys"><span class="keyboard-key">g</span> <span class="keyboard-key">h</span></th>
+						<td class="keyboard-shortcuts-table__action">ホーム画面へ移動</td>
+					</tr>
+					<tr>
+						<th class="keyboard-shortcuts-table__keys"><span class="keyboard-key">g</span> <span class="keyboard-key">a</span></th>
+						<td class="keyboard-shortcuts-table__action">タスク登録画面へ移動</td>
+					</tr>
+					<tr>
+						<th class="keyboard-shortcuts-table__keys"><span class="keyboard-key">g</span> <span class="keyboard-key">l</span></th>
+						<td class="keyboard-shortcuts-table__action">タスクリスト画面へ移動</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</main><!-- .main -->
