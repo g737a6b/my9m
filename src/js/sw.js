cce0826a8e1526883d8bae822cebe56819927776
Hiroyuki Suzuki <github@mofg.net>
2018-09-10 22:03:30 +0900

    Close #5

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,1 +1,27 @@
-self.addEventListener("fetch", function(){});
+self.addEventListener("install", e => {
+	e.waitUntil(caches.open("v1").then(cache => {
+		return cache.addAll([
+			"/",
+			"/?utm_source=homescreen",
+			"/index.html",
+			"/index.html?utm_source=homescreen",
+			"/img/icon-320.png",
+			"/img/icon-192.png",
+			"/css/default.css?1536582556794",
+			"/js/app.js?1536582556794",
+			"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+			"https://fonts.googleapis.com/css?family=Berkshire+Swash"
+		]);
+	}));
+});
+
+self.addEventListener("fetch", e => {
+	e.respondWith(caches.match(e.request).then(response => {
+		return response || fetch(e.request).then(response => {
+			return caches.open("v1").then(cache => {
+				cache.put(e.request, response.clone());
+				return response;
+			});
+		});
+	}));
+});
