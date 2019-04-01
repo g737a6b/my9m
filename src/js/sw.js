9acab65b5f6202517dcb626b2cf3a03c9b0e8303
Hiroyuki Suzuki <github@mofg.net>
2019-04-01 23:17:29 +0900

    Close #11

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,5 +1,7 @@
+const CACHE_CURRENT_VERSION = "my9m-v2";
+
 self.addEventListener("install", e => {
-	e.waitUntil(caches.open("v1").then(cache => {
+	e.waitUntil(caches.open(CACHE_CURRENT_VERSION).then(cache => {
 		return cache.addAll([
 			"/",
 			"/?utm_source=homescreen",
@@ -7,8 +9,8 @@ self.addEventListener("install", e => {
 			"/index.html?utm_source=homescreen",
 			"/img/icon-320.png",
 			"/img/icon-192.png",
-			"/css/default.css?1536582556794",
-			"/js/app.js?1536582556794",
+			"/css/default.css",
+			"/js/app.js",
 			"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 			"https://fonts.googleapis.com/css?family=Berkshire+Swash"
 		]);
@@ -18,10 +20,19 @@ self.addEventListener("install", e => {
 self.addEventListener("fetch", e => {
 	e.respondWith(caches.match(e.request).then(response => {
 		return response || fetch(e.request).then(response => {
-			return caches.open("v1").then(cache => {
+			return caches.open(CACHE_CURRENT_VERSION).then(cache => {
 				cache.put(e.request, response.clone());
 				return response;
 			});
 		});
+	}));
+});
+
+self.addEventListener("activate", e => {
+	e.waitUntil(caches.keys().then(cacheNames => {
+		return Promise.all(cacheNames.map(cacheName => {
+			if( cacheName === CACHE_CURRENT_VERSION ) return;
+			caches.delete(cacheName);
+		}));
 	}));
 });
